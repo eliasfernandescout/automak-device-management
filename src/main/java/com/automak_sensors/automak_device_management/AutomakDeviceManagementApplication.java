2d1b436bc93d0f2b164bbe3192673f6d5c88c9da
eliasfernandescout <eliasfernandescout@gmail.com>
2025-06-22 21:39:16 -0300

    first commit

---
+++ src/main/java/com/automak_sensors/automak_device_management/AutomakDeviceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.automak_sensors.automak_device_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomakDeviceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomakDeviceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automak_sensors/automak_device_management/AutomakDeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automak_sensors.automak_device_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomakDeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
