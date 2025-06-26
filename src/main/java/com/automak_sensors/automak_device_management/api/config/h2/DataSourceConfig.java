906d8077043549eea55e9ce5a70d6dae0a9e6205
eliasfernandescout <eliasfernandescout@gmail.com>
2025-06-26 17:37:41 -0300

    Refactor package structure for H2 configuration classes

--- src/main/java/com/automak_sensors/automak_device_management/api/config/h2/DataSourceConfig.java
+++ src/main/java/com/automak_sensors/automak_device_management/api/config/h2/DataSourceConfig.java
@@ -1,6 +1,5 @@
-package com.automak_sensors.automak_device_management.api.config;
+package com.automak_sensors.automak_device_management.api.config.h2;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/automak_sensors/automak_device_management/api/config/h2/H2ServerConfig.java
+++ src/main/java/com/automak_sensors/automak_device_management/api/config/h2/H2ServerConfig.java
@@ -1,4 +1,4 @@
-package com.automak_sensors.automak_device_management.api.config;
+package com.automak_sensors.automak_device_management.api.config.h2;
 
 import org.h2.tools.Server;
 import org.springframework.context.annotation.Bean;
